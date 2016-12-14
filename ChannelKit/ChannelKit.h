7c5168920b7751593f0316cc7c942bda3b280521
Marc Palluat de Besset <marc.palluatdebesset@hibu.com>
2016-12-14 15:45:35 +0000

    Initial Commit

---
+++ ChannelKit/ChannelKit.h
@@ -0,0 +1,19 @@
+//
+//  ChannelKit.h
+//  ChannelKit
+//
+//  Created by Marc Palluat de Besset on 14/12/2016.
+//  Copyright © 2016 hibu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ChannelKit.
+FOUNDATION_EXPORT double ChannelKitVersionNumber;
+
+//! Project version string for ChannelKit.
+FOUNDATION_EXPORT const unsigned char ChannelKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ChannelKit/PublicHeader.h>
+
+
